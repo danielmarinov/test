de8bf1ef80b15f1b17e15374884852594d3b09f1
danielmarinov <daniel.m.marinov@gmail.com>
2017-07-23 18:33:55 +0300

    Update index.php
    Втора редакция

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+    // втора редакция от GitHub
+?>
 <html>
     <head>
         <meta charset="UTF-8">
